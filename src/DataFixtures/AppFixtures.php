c558071fd0373309e8564d4888535c0a423dea6e
Thibaud <obit2004@hotmail.com>
2025-10-29 10:06:37 +0100

    Security : User + login form, rôle ROLE_ADMIN

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $hashed = $this->hasher->hashPassword($admin, 'adminpass');
+        $admin->setPassword($hashed);
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
