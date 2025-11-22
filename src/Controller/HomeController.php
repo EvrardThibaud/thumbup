695d3b5d7753f1514d37c4e72e913d0179d6344f
Thibaud <thibaud.evrard@cern.ch>
2025-11-22 18:35:56 +0100

    Last thumnails

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Repository\OrderRepository;
+use App\Repository\ThumbnailRepository;
 use App\Enum\OrderStatus;
 
 final class HomeController extends AbstractController
@@ -19,14 +20,18 @@ final class HomeController extends AbstractController
         return $this->render('home/landing.html.twig');
     }
 
-    #[Route('/app', name: 'app_home', methods: ['GET'])]
+     #[Route('/app', name: 'app_home', methods: ['GET'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function app(OrderRepository $orderRepo): Response
-    {
-        $createdOrders  = [];
-        $acceptedOrders = [];
+    public function app(
+        OrderRepository $orderRepo,
+        ThumbnailRepository $thumbRepo
+    ): Response {
+        $createdOrders   = [];
+        $acceptedOrders  = [];
+        $clientThumbnails = [];
 
         if ($this->isGranted('ROLE_ADMIN')) {
+            // Orders en status CREATED
             $createdOrders = $orderRepo->createQueryBuilder('o')
                 ->andWhere('o.status = :created')
                 ->setParameter('created', OrderStatus::CREATED)
@@ -35,6 +40,7 @@ final class HomeController extends AbstractController
                 ->getQuery()
                 ->getResult();
 
+            // Orders en status ACCEPTED (to do)
             $acceptedOrders = $orderRepo->createQueryBuilder('o2')
                 ->andWhere('o2.status = :accepted')
                 ->setParameter('accepted', OrderStatus::ACCEPTED)
@@ -44,9 +50,28 @@ final class HomeController extends AbstractController
                 ->getResult();
         }
 
+        // Vue client : 6 dernières thumbnails liées à ses orders
+        if ($this->isGranted('ROLE_CLIENT') && !$this->isGranted('ROLE_ADMIN')) {
+            /** @var \App\Entity\User $user */
+            $user = $this->getUser();
+            $client = $user?->getClient();
+
+            if ($client) {
+                $clientThumbnails = $thumbRepo->createQueryBuilder('t')
+                    ->join('t.order', 'o')
+                    ->andWhere('o.client = :client')
+                    ->setParameter('client', $client)
+                    ->orderBy('t.createdAt', 'DESC')
+                    ->setMaxResults(6)
+                    ->getQuery()
+                    ->getResult();
+            }
+        }
+
         return $this->render('home/index.html.twig', [
-            'createdOrders'  => $createdOrders,
-            'acceptedOrders' => $acceptedOrders,
+            'createdOrders'    => $createdOrders,
+            'acceptedOrders'   => $acceptedOrders,
+            'clientThumbnails' => $clientThumbnails,
         ]);
     }
 }
